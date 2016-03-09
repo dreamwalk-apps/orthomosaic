a40e085ed167bb7cebfab3674c956e6bb378c361
Alex Hagiopol <alexhagiopol@gmail.com>
2016-03-09 03:48:29 -0500

    Can combine n images.

--- Combiner.py
+++ Combiner.py
@@ -3,11 +3,11 @@ import numpy as np
 import utilities as util
 import geometry as gm
 import pyramidBlending
+import copy
 
 class Combiner:
     def __init__(self,imageList_,dataMatrix_):
         self.imageList = []
-        self.kpList = []
         self.dataMatrix = dataMatrix_
         detector = cv2.ORB()
         for i in range(0,len(imageList_)):
@@ -17,46 +17,61 @@ class Combiner:
             M = gm.computeUnRotMatrix(self.dataMatrix[i,:])
             correctedImage, correctedKP = gm.warpPerspectiveWithPadding(imageList_[i],M,kp)
             self.imageList.append(correctedImage)
-            self.kpList.append(correctedKP)
-            test = cv2.drawKeypoints(correctedImage,correctedKP,color=(0,0,255))
-            util.display("TEST",test)
-
+        self.resultImage = self.imageList[0]
     def createMosaic(self):
         for i in range(1,len(self.imageList)):
-            image = self.imageList[i]
-            M = gm.computeUnRotMatrix(self.dataMatrix[i,:])
-            correctedImage = gm.warpWithPadding(image,M)
-            combinedResult = self.combine(self.result,correctedImage)
-            self.result = combinedResult
-        return self.result
+            self.combine(i)
+        return self.resultImage
 
-    def combine(self, index1, index2):
+    def combine(self, index2):
         '''
-        :param index1: index of self.imageList and self.kpList to combine
-        :param index2: index of self.imageList and self.kpList to combine
-        :return:
+        :param index2: index of self.imageList and self.kpList to combine with self.referenceImage and self.referenceKeypoints
+        :return: combination of reference image and image at index 2
         '''
-        
 
-        '''Feature detection and matching'''
-        detector = cv2.ORB()
-        matcher = cv2.BFMatcher(cv2.NORM_HAMMING2, crossCheck=True)
+        image1 = copy.copy(self.imageList[index2 - 1])
+        image2 = copy.copy(self.imageList[index2])
+        #kp1 = copy.copy(self.referenceKP)
+        #kp2 = copy.copy(self.kpList[index2])
+
+        '''Descriptor computation and matching'''
+        detector = cv2.ORB(1000)
         gray1 = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
-        kp1, descriptors1 = detector.detectAndCompute(gray1,None)
+        ret1, mask1 = cv2.threshold(gray1,1,255,cv2.THRESH_BINARY)
+        kp1, descriptors1 = detector.detectAndCompute(gray1,mask1)
+
         gray2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
-        kp2, descriptors2 = detector.detectAndCompute(gray2,None)
+        ret2, mask2 = cv2.threshold(gray2,1,255,cv2.THRESH_BINARY)
+        kp2, descriptors2 = detector.detectAndCompute(gray2,mask2)
+
+        '''
+        if index2 > 1:
+            gray_p = cv2.cvtColor(self.imageList[index2-1],cv2.COLOR_BGR2GRAY)
+            ret_p, mask_p = cv2.threshold(gray_p,1,255,cv2.THRESH_BINARY)
+            kp_p, descriptors_p = detector.detectAndCompute(gray_p,mask_p)
+
+            kp1 = kp1 + kp_p
+            descriptors2 = descriptors2 + descriptors_p
+        '''
+        test = cv2.drawKeypoints(image1,kp1,color=(0,0,255))
+        util.display("TEST",test)
+        test = cv2.drawKeypoints(image2,kp2,color=(0,0,255))
+        util.display("TEST",test)
+
+        matcher = cv2.BFMatcher(cv2.NORM_HAMMING2, crossCheck=True)
         matches = matcher.match(descriptors2,descriptors1)
         matchDrawing = util.drawMatches(gray1,kp1,gray2,kp2,matches)
         util.display("matches",matchDrawing)
-        cv2.imwrite("matchDrawing.png",matchDrawing)
+
         src_pts = np.float32([ kp2[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
         dst_pts = np.float32([ kp1[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
 
         '''Compute Affine Transform'''
         A = cv2.estimateRigidTransform(src_pts,dst_pts,fullAffine=False)
-        print "A"
-        print A
-
+        if A == None:
+            print "FINDING AFFINE TRANSFORM FAILED. ATTEMPT FULL HOMOGRAPHY"
+            HomogResult = cv2.findHomography(src_pts,dst_pts,method=cv2.RANSAC)
+            H = HomogResult[0]
         '''Compute 4 Image Corners Locations'''
         height1,width1 = image1.shape[:2]
         height2,width2 = image2.shape[:2]
@@ -66,32 +81,46 @@ class Combiner:
         for i in range(0,4):
             cornerX = corners2[i,0]
             cornerY = corners2[i,1]
-            warpedCorners2[i,0] = A[0,0]*cornerX + A[0,1]*cornerY + A[0,2]
-            warpedCorners2[i,1] = A[1,0]*cornerX + A[1,1]*cornerY + A[1,2]
+            if A != None:
+                warpedCorners2[i,0] = A[0,0]*cornerX + A[0,1]*cornerY + A[0,2]
+                warpedCorners2[i,1] = A[1,0]*cornerX + A[1,1]*cornerY + A[1,2]
+            else:
+                warpedCorners2[i,0] = (H[0,0]*cornerX + H[0,1]*cornerY + H[0,2])/(H[2,0]*cornerX + H[2,1]*cornerY + H[2,2])
+                warpedCorners2[i,1] = (H[1,0]*cornerX + H[1,1]*cornerY + H[1,2])/(H[2,0]*cornerX + H[2,1]*cornerY + H[2,2])
         allCorners = np.concatenate((corners1, warpedCorners2), axis=0)
         [xMin, yMin] = np.int32(allCorners.min(axis=0).ravel() - 0.5)
         [xMax, yMax] = np.int32(allCorners.max(axis=0).ravel() + 0.5)
 
-        '''Compute Image Alignment'''
+        '''Compute Image Alignment and Keypoint Alignment'''
         translation = np.float32(([1,0,-1*xMin],[0,1,-1*yMin],[0,0,1]))
-        warpedImage1 = cv2.warpPerspective(image1, translation, (xMax-xMin, yMax-yMin))
-        warpedImageTemp = cv2.warpPerspective(image2, translation, (xMax-xMin, yMax-yMin))
-        warpedImage2 = cv2.warpAffine(warpedImageTemp, A, (xMax-xMin, yMax-yMin))
-        #returnWarpedImage2 = np.copy(warpedImage2)
+        warpedResImg = cv2.warpPerspective(self.resultImage, translation, (xMax-xMin, yMax-yMin))
+        if A == None:
+            fullTransformation = np.dot(translation,H)
+            warpedImage2 = cv2.warpPerspective(image2, fullTransformation, (xMax-xMin, yMax-yMin))
+        else:
+            warpedImageTemp = cv2.warpPerspective(image2, translation, (xMax-xMin, yMax-yMin))
+            warpedImage2 = cv2.warpAffine(warpedImageTemp, A, (xMax-xMin, yMax-yMin))
+        self.imageList[index2] = copy.copy(warpedImage2) #crucial: update old images for future feature extractions
 
-        warpedGray1 = cv2.warpPerspective(gray1, translation, (xMax-xMin, yMax-yMin))
-        warpedGrayTemp = cv2.warpPerspective(gray2, translation, (xMax-xMin, yMax-yMin))
-        warpedGray2 = cv2.warpAffine(warpedGrayTemp, A, (xMax-xMin, yMax-yMin))
+        '''
+        #crucial: update old images for future feature extractions
+        for i in range(0,index2 + 1)
+            self.imageList[i] =
+        '''
+        resGray = cv2.cvtColor(self.resultImage,cv2.COLOR_BGR2GRAY)
+        warpedResGray = cv2.warpPerspective(resGray, translation, (xMax-xMin, yMax-yMin))
 
         '''Compute Mask for Image Combination'''
-        ret, mask1 = cv2.threshold(warpedGray1,1,255,cv2.THRESH_BINARY_INV)
-        mask1 = np.float32(mask1)/255
+        ret, mask1 = cv2.threshold(warpedResGray,1,255,cv2.THRESH_BINARY_INV)
+        mask3 = np.float32(mask1)/255
 
-        warpedImage2[:,:,0] = warpedImage2[:,:,0]*mask1
-        warpedImage2[:,:,1] = warpedImage2[:,:,1]*mask1
-        warpedImage2[:,:,2] = warpedImage2[:,:,2]*mask1
+        warpedImage2[:,:,0] = warpedImage2[:,:,0]*mask3
+        warpedImage2[:,:,1] = warpedImage2[:,:,1]*mask3
+        warpedImage2[:,:,2] = warpedImage2[:,:,2]*mask3
 
-        result = warpedImage1 + warpedImage2
+        result = warpedResImg + warpedImage2
+        #self.referenceImage = result
+        self.resultImage = result
         util.display("result",result)
         return result
 
--- ImageMosaic.py
+++ ImageMosaic.py
@@ -9,8 +9,8 @@ fileName = "datasets/imageData.txt"
 imageDirectory = "datasets/images/"
 dataMatrix, allImages = util.importData(fileName, imageDirectory)
 myCombiner = Combiner.Combiner(allImages,dataMatrix)
-
-
+result = myCombiner.createMosaic()
+util.display("RESULT",result)
 
 
 
